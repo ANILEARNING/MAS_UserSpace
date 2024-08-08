b704f6a3342c86b6401aec6c5ebb66d57a0fa967
ANILEARNING <100782934+ANILEARNING@users.noreply.github.com>
2024-08-08 22:09:13 +0530

    Update consultants.py
    added bio

--- consultants.py
+++ consultants.py
@@ -11,6 +11,7 @@ def create_consultant_box(name, photo_path, role, whatsapp_number):
         with col2:
             st.subheader(name)
             st.write(role)
+            st.write(bio)
             
             whatsapp_url = f"https://wa.me/{whatsapp_number}"
             st.markdown(f"[![Message](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)]({whatsapp_url})")
@@ -35,20 +36,31 @@ def main():
             "name": "Anish S",
             "photo": "assets/anish.jpg",
             "role": "Full Stack Data Scientist - Cricket",
-            "whatsapp": "919384972461"
+            "whatsapp": "919384972461",
+            "bio":"Creator of innovative cricket solutions through data and technology; and one of my work in your hand, explore how my expertise can transform your game."
         },
         {
             "name": "Ankit Gujare",
             "photo": "assets/ankit.jpg",
             "role": "Cricket Social Media Expert",
-            "whatsapp": "919967868733"
+            "whatsapp": "919967868733",
+            "bio":"I can help you grow your Cricket Page/Channel"
         },
         {
             "name": "Radhika patle",
             "photo": "assets/radhika.jpg",
             "role": "Injury Prevention and Rehabilitation",
-            "whatsapp": "919752622331"
+            "whatsapp": "919752622331",
+            "bio": ""
         },
+        {
+            "name": "Aniketh Kotamraju",
+            "photo": "assets/radhika.jpg",
+            "role": "Content Researcher",
+            "whatsapp": "917989072868"
+            "bio":""
+        }
+        
         # Add more consultants here if needed
     ]
 
@@ -59,6 +71,7 @@ def main():
                 consultant["name"],
                 consultant["photo"],
                 consultant["role"],
+                consultant["bio"],
                 consultant["whatsapp"]
             )
             st.markdown('</div>', unsafe_allow_html=True)
